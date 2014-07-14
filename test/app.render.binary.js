1241dcb18303705e165a4b39c0187119e415147c
Phil DeJarnett <phil@overzealous.com>
2014-07-14 15:03:19 -0400

    Reduced risk of false positives for inserting the script by making the html regex stricter, adding many more filetypes to the default exclusion list, and adjusting the html/rules tests.  Fixes shakyShane/browser-sync#198

--- test/app.render.binary.js
+++ test/app.render.binary.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var fs = require("fs");
+var path = require("path");
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -10,7 +11,14 @@ app.configure("development", function () {
     // live reload script
     var livereload = require("../index.js");
     app.use(livereload({
-        ignore: [".woff", ".flv"]
+        rules: [
+            {
+                match: /<[^>]*>/i,
+                fn: function (w) {
+                    return w+"matcher";
+                }
+            }
+        ]
     }));
 });
 // load the routes
@@ -30,52 +38,53 @@ if (!module.parent) {
 var request = require("supertest");
 var assert = require("assert");
 
-describe("GET /favicon.ico", function () {
-    it("respond with inserted script", function (done) {
-        request(app)
-            .get("/favicon.ico")
-            .set("Accept", "text/html")
-            .expect(200)
-            .parse(function (res) {
-                res.chunk = "";
-                res.on("data", function (chunk) {
-                    res.chunk += chunk;
-                });
-                res.on("end", function () {
-                    assert(res.chunk.length > 0);
+
+var notModified = ["binary.unknown", "blank.pdf", "client.js", "favicon.ico", "font.ttf", "icons.jpg", "json-masquerading-as.html", "no-ext-json-data", "node.png", "tux.svg"];
+var modified = ["static.html", "no-ext-html"];
+
+notModified.forEach(function(file) {
+    var orig = fs.readFileSync(path.join("test/fixtures", file));
+    describe("GET \"/"+file+"\"", function () {
+        it("responds without an inserted script", function (done) {
+            request(app)
+                .get("/"+file)
+                .set("Accept", "text/html")
+                .expect(200)
+                .parse(function binaryParser(res, callback) {
+                    res.setEncoding('binary');
+                    res.data = '';
+                    res.on('data', function (chunk) {
+                        res.data += chunk;
+                    });
+                    res.on('end', function () {
+                        callback(null, new Buffer(res.data, 'binary'));
+                    });
+                })
+                .end(function (err, res) {
+                    assert(!err);
+                    assert.ok(Buffer.isBuffer(res.body));
+                    assert.equal(res.body.length, orig.length, "equal length");
+                    assert.equal(res.body.toString(), orig.toString(), "equal contents");
                     done();
                 });
-            })
-            .end(function (err) {
-                assert(!err);
-                if (err) {
-                    return done(err);
-                }
-            });
+        });
     });
 });
 
-describe("GET /font.ttf", function () {
-    it("respond with inserted script", function (done) {
-        request(app)
-            .get("/font.ttf")
-            .set("Accept", "text/html")
-            .expect(200)
-            .parse(function (res) {
-                res.chunk = "";
-                res.on("data", function (chunk) {
-                    res.chunk += chunk;
-                });
-                res.on("end", function () {
-                    assert(res.chunk.length > 0);
+modified.forEach(function(file) {
+    var orig = fs.readFileSync(path.join("test/fixtures", file)).toString();
+    describe("GET \"/"+file+"\"", function () {
+        it("responds with an inserted script", function (done) {
+            request(app)
+                .get("/"+file)
+                .set("Accept", "text/html")
+                .expect(200)
+                .end(function (err, res) {
+                    assert(!err);
+                    assert(res.text.length > orig.length, "longer content length");
+                    assert.equal(res.text.match(/matcher/).length, 1);
                     done();
                 });
-            })
-            .end(function (err) {
-                assert(!err);
-                if (err) {
-                    return done(err);
-                }
-            });
+        });
     });
 });
