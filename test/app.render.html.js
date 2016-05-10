76a4874b55f9bf46f4414fcfa47ee9256136dc22
Shane Osbourne <shane.osbourne8@gmail.com>
2016-05-10 06:52:50 +0100

    fix: never match file extensions in query strings - re: https://github.com/BrowserSync/browser-sync/issues/1092

--- test/app.render.html.js
+++ test/app.render.html.js
@@ -49,4 +49,27 @@ describe("GET /dummies", function () {
                 done();
             });
     });
+    it("allows .json etc in query params", function (done) {
+        request(app)
+            .get("/multi.html?file=*.json")
+            .set("Accept", "text/html")
+            .expect(200)
+            .end(function (err, res) {
+                var count1 = 0;
+                var count2 = 0;
+
+                res.text.replace(/<<SHANE/g, function () {
+                    count1 += 1;
+                });
+
+                res.text.replace(/SHANE/g, function () {
+                    count2 += 1;
+                });
+
+                assert.equal(count1, 1);
+                assert.equal(count2, 150);
+
+                done();
+            });
+    });
 });
